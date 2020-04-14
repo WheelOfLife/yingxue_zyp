c3963f1a7dbcb3b6d4bef6860b5850a8d7b297be
WheelOfLife <62942053+WheelOfLife@users.noreply.github.com>
2020-04-14 10:50:03 +0800

    Update TestGit.java

--- src/test/java/com/baizhi/zyp/TestGit.java
+++ src/test/java/com/baizhi/zyp/TestGit.java
@@ -8,5 +8,20 @@ package com.baizhi.zyp;
  * Date:  2020/4/14
  */
 public class TestGit {
+  @Resource
+    RedisTemplate redisTemplate;
+    @Resource
+    StringRedisTemplate stringRedisTemplate;
+    @Test
+    public void redisTest(){
+      /*  Set keys = redisTemplate.keys("*");
+        for (Object key : keys) {
+            System.out.println(key);
+        }*/
+     // redisTemplate.opsForValue().set("namee","aaa");
+        ValueOperations valueOperations = redisTemplate.opsForValue();
+        Object name = valueOperations.get("name");
+        System.out.println(name);
+    }
     
 }
